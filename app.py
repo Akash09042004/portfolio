a28620c3cf92815bd77ab15ada80b0ef0de196fa
Akash09042004 <akashnagarajan001@gmail.com>
2025-11-06 06:24:46 +0530

    Your commit message here

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ def contact():
 # Run Flask app on Render
 # -----------------------------
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
